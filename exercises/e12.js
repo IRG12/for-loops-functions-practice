7a8dbbfd1aabf4e5b5a832f76d8694b5cf1e4d1c
Isaiah G <isaiahrg12@gmail.com>
2023-04-04 13:42:23 -0500

    Practicing For Loops
    Passed tests for 'e12.js'

--- exercises/e12.js
+++ exercises/e12.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 12
 // Return an array of all deposits greater than 100
 // Array example: bankAccounts in /data/data.js
@@ -7,9 +6,38 @@
 export function getAllDepositsGreaterThanOneHundred(array) {
   // Your code goes here...
 
+  let result = [];
+  // traverse array
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i])
+    // store withdrawals to a variable
+    let accountsDeposits = array[i].deposits;
+    let sum = 0;
+    // console.log(accountsDeposits)
+    // if an account's withdrawal exist
+    if (accountsDeposits !== undefined) {
+      // console.log(accountsDeposits)
+      //traverse accounts that have withdrawals
+      for (let j = 0; j < accountsDeposits.length; j++) {
+        // console.log(accountDeposits[j])
+        if (accountsDeposits[j] > 100) {
+          let accountDeposits = accountsDeposits[j];
+          console.log(accountDeposits);
+          // sum each accounts withdrawals
+          // sum += deposit[j];
+          // console.log(sum)
+          result.push(accountDeposits);
+        }
+      }
+      // console.log(`Sum of nested array ${i + 1}: ${sum}`);
+      // push sum of each account to empty array named 'result'
+      // if an account's withdrawal does NOT exist
+    }
+  }
+  // answer
+  console.log(result);
+  return result;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
