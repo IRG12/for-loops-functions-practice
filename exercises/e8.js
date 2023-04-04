7b9921e64e13a2dd9cfa5b8f95d462ddfe3c1784
Isaiah G <isaiahrg12@gmail.com>
2023-04-04 06:54:46 -0500

    Practicing For Loops
    Passed tests for 'e8.js'

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,15 +1,33 @@
-
 // EXERCISE 8
 // Return an array with a bank account object with the greatest balance value
 // Array example: bankAccounts in /data/data.js
 // getClientWithGreatestBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
 export function getClientWithGreatestBalance(array) {
-  // Your code goes here...
+  let balance = [];
+  let maxBalance = array[0].balance;
+  let bankAccount = {};
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i].name);
+    // console.log(array[i].balance);
 
+    if (array[i].balance > 0) {
+      // console.log(array[i].name, array[i].balance)
+      if (array[i].balance > maxBalance) {
+        bankAccount.id = array[i].id;
+        bankAccount.name = array[i].name;
+        bankAccount.balance = array[i].balance;
+        bankAccount.deposits = array[i].deposits;
+        bankAccount.withdrawals = array[i].withdrawals;
+        // console.log(object)
+        balance.push(bankAccount);
+      }
+    }
+  }
+
+  console.log([balance[0]], "balance");
+  return [balance[0]];
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
