4fe460fccf97628014de72cae628fde6929a0c0b
Isaiah G <isaiahrg12@gmail.com>
2023-04-04 08:06:02 -0500

    Practicing For Loops
    Refactored and Passed tests for 'e9.js'

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,15 +1,33 @@
-
 // EXERCISE 9
 // Return an array of bank accounts with balance over $100
 // Array example: bankAccounts in /data/data.js
 // getClientsWithBalanceOverOneHundred(array) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithBalanceOverOneHundred(array) {
-  // Your code goes here...
-
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].balance > 100) {
+      // console.log(array[i].name, array[i].balance)
+      const acount = array[i];
+      const bankAccount = {
+        id: acount.id,
+        name: acount.name,
+        balance: acount.balance,
+        deposits: acount.deposits,
+        withdrawals: acount.withdrawals,
+      };
+      for (let key in acount) {
+        if (acount[key] !== undefined) {
+          bankAccount[key] = acount[key];
+        }
+      }
+      result.push(bankAccount);
+      // console.log(bankAccount)
+    }
+  }
+  console.log(result);
+  return result;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
