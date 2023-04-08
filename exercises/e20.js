ab3273e6953ea92f203208a53f375c86e04b065c
Isaiah G <isaiahrg12@gmail.com>
2023-04-08 17:17:19 -0500

    Practicing For Loops
    Passed tests for 'e20.js'

--- exercises/e20.js
+++ exercises/e20.js
@@ -5,7 +5,32 @@
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
 export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
+  let array2 = [];
+  let array3 = [];
+  let result = [];
+  let letter = "a";
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i])
+    let hasLetter = false;
+    for (let j = 0; j < array[i].length; j++) {
+      // console.log(array[i][j])
+      if (letter === array[i][j]) {
+        hasLetter = true;
+        break;
+      }
+    }
+    if (hasLetter) {
+      array2.push(array[i]);
+    } else {
+      array3.push(array[i]);
+    }
+  }
+  // console.log(array2)
+  result.push(array2);
+  // console.log(array3)
+  result.push(array3);
+  // console.log(result);
+  return result;
 }
 
 // === TEST YOURSELF ===
